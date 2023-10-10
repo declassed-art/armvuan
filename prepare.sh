e6e2f56c1c837f25faa06b7bf7dbf0f78d5db503
Axy <declassed.art@gmail.com>
2023-10-10 19:16:02 +0100

    Added missing distributions init.

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,7 @@ SRC=`dirname $0`
 SRC=`realpath "${SRC}"`
 
 cp "${SRC}"/cli-armvuan.sh "${SRC}"/armbian/lib/functions/cli/
+cp -a "${SRC}"/distributions/* "${SRC}"/armbian/config/distributions/
 pushd "${SRC}"/armbian
 patch -Np1 <"${SRC}"/armbian.patch
 popd
